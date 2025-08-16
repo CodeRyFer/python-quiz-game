7ed2406f763df77c8279cbe9b0f6c8fb644661a3
Ryan Fehr <coderry4@gmail.com>
2025-08-15 19:38:09 -0500

    Adds quiz questions

--- src/quiz_game.py
+++ src/quiz_game.py
@@ -6,3 +6,28 @@ if playing != "yes":
     quit()
 
 print("Ok! Let's play! :)")
+
+answer = input("What does CPU stand for? ")
+if answer == "central processing unit":
+    print("Correct! Nice job!")
+else:
+    print("Incorrect!")
+####################################################
+answer = input("What does GPU stand for? ")
+if answer == "graphics processing unit":
+    print("Correct! Nice job!")
+else:
+    print("Incorrect!")
+####################################################
+answer = input("What does RAM stand for? ")
+if answer == "random access memory":
+    print("Correct! Nice job!")
+else:
+    print("Incorrect!")
+####################################################
+answer = input("What does PSU stand for? ")
+if answer == "power supply unit":
+    print("Correct! Nice job!")
+else:
+    print("Incorrect!")
+
